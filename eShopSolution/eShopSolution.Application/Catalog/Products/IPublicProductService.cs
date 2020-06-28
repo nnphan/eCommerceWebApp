a9aa8bb5324206d8b20cdd5883c7314521bfe5e2
nnphan <nnphan20041997@gmail.com>
2020-06-28 23:00:55 +0700

    commit application layer

---
+++ eShopSolution/eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,14 @@
+﻿using eShopSolution.Application.Catalog.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Public;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        PagedResult<ProductViewModel> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
---
+++ eShopSolution/eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -0,0 +1,19 @@
+﻿using eShopSolution.Application.Catalog.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class PublicProductService : IPublicProductService
+    {
+        public int CategoryId { get; set; }
+
+        public PagedResult<ProductViewModel> GetAllByCategoryId(Dtos.Public.GetProductPagingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
